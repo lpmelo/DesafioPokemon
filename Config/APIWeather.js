9076152ddc23980d21703cecd0f2ba175f8daa78
Lucas Pereira Melo <lpmelo10@gmail.com>
2021-02-03 18:41:19 -0300

    Updates de funcionamento API Weather

--- Config/APIWeather.js
+++ Config/APIWeather.js
@@ -1,23 +1,35 @@
-var nc = "";
-class peganome {
-    constructor(nomecidade){ 
-      this.nc = nomecidade;
-      var key ="6ce079aa71a79c95ed661ab729f8f323";
-      var urlstr = "https://api.openweathermap.org/data/2.5/weather?q="+nomecidade+"&appid="+key;
-      const mostrardados = (result)=>{ // Utilizando Arrow Function para economizar linhas, poderia ser uma function padrão, ex: const mostrardados = function mostrardados(result)
-        for(const campo in result){
-          //if(document.que)
-          console.log(campo);
-        }
-      }
-      const options={
-      method: 'GET',
-      mode: 'cors',
-      cache: 'default'
+class pegacidade {
+  constructor(nomecidade) {
+    this.nomecidade = nomecidade;
+    var key = "6ce079aa71a79c95ed661ab729f8f323";
+    var urlstr = "https://api.openweathermap.org/data/2.5/weather?q=" + nomecidade + "&appid=" + key +"&units=metric";
+    
+    
+    fetch(`${urlstr}`, options)
+      .then(response => {
+        response.json()
+          .then(data => mostrardados(data))
+      })
+      .catch(e => console.log('Deu erro: ' + e, message));
+  }
 }
-fetch(`${urlstr}`,options)
-  .then(response =>{ response.json()  
-    .then(data => console.log(mostrardados(data)))})
-  .catch(e => console.log('Deu erro: '+e,message));
-    }
- }
+
+function mostrardados(data){
+  t = data;
+  c = data;
+if (t){
+  t = t.main.temp.toString(); 
+}
+if (c){
+  c = c.weather[0];
+  console.log(c);
+}
+}
+
+var t= null;
+
+const options = {
+  method: 'GET',
+  mode: 'cors',
+  cache: 'default'
+}
