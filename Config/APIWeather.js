4ce29431ce7e5c9a2212ce45629c22d6579aa3cc
Lucas Pereira Melo <lpmelo10@gmail.com>
2021-02-04 17:57:56 -0300

    Update APIWeather totalmente funcional

--- Config/APIWeather.js
+++ Config/APIWeather.js
@@ -1,32 +1,36 @@
-class pegacidade {
-  constructor(nomecidade) {
-    this.nomecidade = nomecidade;
+class PegaCidade {
+  constructor(nomeCidade) {
+    this.nomecidade = nomeCidade;
     var key = "6ce079aa71a79c95ed661ab729f8f323";
-    var urlstr = "https://api.openweathermap.org/data/2.5/weather?q=" + nomecidade + "&appid=" + key +"&units=metric";
-    
-    
+    var urlstr = "https://api.openweathermap.org/data/2.5/weather?q=" + nomeCidade + "&appid=" + key + "&units=metric";
+
+
     fetch(`${urlstr}`, options)
       .then(response => {
         response.json()
-          .then(data => mostrardados(data))
+          .then(data => mostrarDados(data))
       })
       .catch(e => console.log('Deu erro: ' + e, message));
   }
 }
-function mostrardados(data){
-  teste = data;
-  c = data;
-if (teste){
-  teste = teste.main.temp.toString(); 
-  console.log(teste);
+function mostrarDados(data) {
+  let t, weather, main,chuva;
+  if (data) {
+    t = data.main.temp;
+    console.log(t);
+    weather = data.weather;
+    weather.map((index) => {
+      main = index.main
+      if (main == "Rain") {
+        chuva = "Sim";
+      } else { chuva = "Não"; }
+      console.log(main);
+      console.log(chuva);
+    });
+    document.getElementById('main').innerHTML = t;
+    document.getElementById('weather').innerHTML = chuva;
+  }
 }
-if (c){
-  c = c.weather[0];
-  console.log(c);
-}
-}
-
-var t= null;
 
 const options = {
   method: 'GET',
